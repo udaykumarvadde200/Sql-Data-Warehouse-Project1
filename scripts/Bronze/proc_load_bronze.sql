bc591a8104a05cf692c7370a4c5220b1494083d2
udaykumarvadde200 <udaykumarvadde210@gmail.com>
2026-03-26 21:56:27 +0530

    Update proc_load_bronze.sql

--- scripts/Bronze/proc_load_bronze.sql
+++ scripts/Bronze/proc_load_bronze.sql
@@ -1,47 +1,127 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
-	BEGIN TRY
-		SET @batch_start_time = GETDATE();
+    DECLARE 
+        @batch_start_time DATETIME,
+        @base_path NVARCHAR(500),
+        @crm_path NVARCHAR(500),
+        @erp_path NVARCHAR(500),
+        @sql NVARCHAR(MAX);
 
-		PRINT 'Loading Bronze Layer';
+    BEGIN TRY
+        SET @batch_start_time = GETDATE();
 
-		-- CRM Tables
-		TRUNCATE TABLE bronze.crm_cust_info;
-		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\uday kumar\Desktop\DATAWAREOUSE PROJECT\DaraWarepuse\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-		WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', TABLOCK);
+        PRINT '=========================================';
+        PRINT 'Loading Bronze Layer';
+        PRINT '=========================================';
 
-		TRUNCATE TABLE bronze.crm_prd_info;
-		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\uday kumar\Desktop\DATAWAREOUSE PROJECT\DaraWarepuse\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-		WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', TABLOCK);
+        -- =========================================
+        -- Update this base path before execution
+        -- =========================================
+        SET @base_path = 'REPLACE_WITH_YOUR_PROJECT_PATH\datasets\';
+        SET @crm_path  = @base_path + 'source_crm\';
+        SET @erp_path  = @base_path + 'source_erp\';
 
-		TRUNCATE TABLE bronze.crm_sales_details;
-		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\uday kumar\Desktop\DATAWAREOUSE PROJECT\DaraWarepuse\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-		WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', TABLOCK);
+        -- =========================================
+        -- Load CRM Tables
+        -- =========================================
 
-		-- ERP Tables
-		TRUNCATE TABLE bronze.erp_loc_a101;
-		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\uday kumar\Desktop\DATAWAREOUSE PROJECT\DaraWarepuse\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-		WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', TABLOCK);
+        PRINT 'Loading bronze.crm_cust_info...';
+        TRUNCATE TABLE bronze.crm_cust_info;
 
-		TRUNCATE TABLE bronze.erp_cust_az12;
-		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\uday kumar\Desktop\DATAWAREOUSE PROJECT\DaraWarepuse\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-		WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', TABLOCK);
+        SET @sql = '
+        BULK INSERT bronze.crm_cust_info
+        FROM ''' + @crm_path + 'cust_info.csv''
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = '','',
+            ROWTERMINATOR = ''0x0A'',
+            TABLOCK
+        );';
+        EXEC(@sql);
 
-		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\uday kumar\Desktop\DATAWAREOUSE PROJECT\DaraWarepuse\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-		WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', TABLOCK);
+        PRINT 'Loading bronze.crm_prd_info...';
+        TRUNCATE TABLE bronze.crm_prd_info;
 
-		PRINT 'Loading Completed';
+        SET @sql = '
+        BULK INSERT bronze.crm_prd_info
+        FROM ''' + @crm_path + 'prd_info.csv''
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = '','',
+            ROWTERMINATOR = ''0x0A'',
+            TABLOCK
+        );';
+        EXEC(@sql);
 
-	END TRY
-	BEGIN CATCH
-		PRINT 'ERROR: ' + ERROR_MESSAGE();
-	END CATCH
+        PRINT 'Loading bronze.crm_sales_details...';
+        TRUNCATE TABLE bronze.crm_sales_details;
+
+        SET @sql = '
+        BULK INSERT bronze.crm_sales_details
+        FROM ''' + @crm_path + 'sales_details.csv''
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = '','',
+            ROWTERMINATOR = ''0x0A'',
+            TABLOCK
+        );';
+        EXEC(@sql);
+
+        -- =========================================
+        -- Load ERP Tables
+        -- =========================================
+
+        PRINT 'Loading bronze.erp_loc_a101...';
+        TRUNCATE TABLE bronze.erp_loc_a101;
+
+        SET @sql = '
+        BULK INSERT bronze.erp_loc_a101
+        FROM ''' + @erp_path + 'loc_a101.csv''
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = '','',
+            ROWTERMINATOR = ''0x0A'',
+            TABLOCK
+        );';
+        EXEC(@sql);
+
+        PRINT 'Loading bronze.erp_cust_az12...';
+        TRUNCATE TABLE bronze.erp_cust_az12;
+
+        SET @sql = '
+        BULK INSERT bronze.erp_cust_az12
+        FROM ''' + @erp_path + 'cust_az12.csv''
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = '','',
+            ROWTERMINATOR = ''0x0A'',
+            TABLOCK
+        );';
+        EXEC(@sql);
+
+        PRINT 'Loading bronze.erp_px_cat_g1v2...';
+        TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+        SET @sql = '
+        BULK INSERT bronze.erp_px_cat_g1v2
+        FROM ''' + @erp_path + 'px_cat_g1v2.csv''
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = '','',
+            ROWTERMINATOR = ''0x0A'',
+            TABLOCK
+        );';
+        EXEC(@sql);
+
+        PRINT '=========================================';
+        PRINT 'Bronze Layer Loading Completed';
+        PRINT '=========================================';
+    END TRY
+    BEGIN CATCH
+        PRINT '=========================================';
+        PRINT 'ERROR OCCURRED DURING BRONZE LOAD';
+        PRINT 'Error Message: ' + ERROR_MESSAGE();
+        PRINT '=========================================';
+    END CATCH
 END;
+GO
